71959c520b68eb69460b895f9d55ba7df37be6bc
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-16 11:11:51 -0400

    Completed assignment

---
+++ artist.rb
@@ -0,0 +1,27 @@
+class Artist < ActiveRecord::Base
+    # attr_accessor :name, :genre, :age, :hometown
+    # attr_reader :id
+
+    # def initialize(id: nil, name:, genre:, age:, hometown:)
+    #     @id = id
+    #     @name = name
+    #     @genre = genre
+    #     @age = age
+    #     @hometown = hometown
+    # end
+
+    # self.column_names.each do |col_name|
+    #     attr_accessor col_name.to_sym
+    # end
+
+    # attr_accessor :name, :genre, :age, :hometown
+
+    # def initialize(options={})
+    #     options.each do |property, value|
+    #         self.send("#{property}=", value)
+    #     end
+    # end
+
+
+
+end
